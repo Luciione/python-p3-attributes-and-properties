f25d7515c3e8584b6cc5086c0bc0329e46427f55
+ <lucy.njoroge1@student.moringaschool.com>
2023-08-25 02:45:19 +0300

    completed

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,30 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    approved_jobs = ["Doctor", "Engineer", "Teacher", "Artist", "Chef"]
+
+    def __init__(self, name, job):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) < 25:
+            self._name = value.title()
+        else:
+            print("Name must be a string under 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in Person.approved_jobs:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
